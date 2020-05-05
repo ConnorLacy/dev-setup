69cf486c3a5c28b3fbc5af22c28bdb69aa82fb37
Connor Lacy <lacyconnor1409@gmail.com>
2020-05-05 17:03:36 -0400

    refactored dir

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 GIT_DIR=$HOME/dev
-SETUP_DIR=$GIT_DIR/personal/zsh
+SETUP_DIR=$GIT_DIR/dev-setup
 
 function antibodyBundle() {
   antibody bundle < $SETUP_DIR/zsh_plugins.txt > $SETUP_DIR/.zsh_plugins.sh
